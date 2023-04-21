d43715d785eceb880f10eff50c02065b4fa0bab4
gh <gorgulius.hatcher@gmail.com>
2023-04-22 00:05:20 +0200

    Create the necessary Dto for schema validation and mapping return object

---
+++ BoatAppApi/Dtos/BoatList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BoatApi.Dtos
+{
+    /// <summary>
+    /// DTO used to return a list of boats.
+    /// </summary>
+    public class BoatListDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+        public int Length { get; set; }
+        public string Brand { get; set; }
+        public int Year { get; set; }
+        public string EngineType { get; set; }
+        public int FuelCapacity { get; set; }
+        public int WaterCapacity { get; set; }
+    }
+}
---
+++ BoatAppApi/Dtos/UpdateBoatDto.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BoatApi.Dtos
+{
+    /// <summary>
+    /// DTO used to update an existing boat entity.
+    /// </summary>
+    public class UpdateBoatDto
+    {
+        /// <summary>
+        /// The name of the boat.
+        /// </summary>
+        [Required]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The description of the boat.
+        /// </summary>
+        [Required]
+        public string Description { get; set; }
+
+        /// <summary>
+        /// The type of boat.
+        /// </summary>
+        public string Type { get; set; }
+
+        /// <summary>
+        /// The length of the boat.
+        /// </summary>
+        public int Length { get; set; }
+
+        /// <summary>
+        /// The brand of the boat.
+        /// </summary>
+        public string Brand { get; set; }
+
+        /// <summary>
+        /// The year the boat was built.
+        /// </summary>
+        public int Year { get; set; }
+
+        /// <summary>
+        /// The type of engine installed in the boat.
+        /// </summary>
+        public string EngineType { get; set; }
+
+        /// <summary>
+        /// The fuel capacity of the boat.
+        /// </summary>
+        public int FuelCapacity { get; set; }
+
+        /// <summary>
+        /// The water capacity of the boat.
+        /// </summary>
+        public int WaterCapacity { get; set; }
+    }
+}
